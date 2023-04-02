e4ef92bd4eb62ce4482d0b55573450dbe0ac746d
Vaibhav Markad <124672523+vaibhavmarkad@users.noreply.github.com>
2023-04-03 01:11:30 +0530

    Ex1::

---
+++ GroupD/src/Example/Ex1.java
@@ -0,0 +1,23 @@
+package exxpression;
+
+public class Ex1 {
+	public static void main(String args[]) {
+		String st = "India";
+		int i, length;
+		int counters[] = new int[150];
+
+		length = st.length();
+
+		for (i = 0; i < length; i++) {
+			counters[(int) st.charAt(i)]++;
+		}
+
+		for (i = 0; i < 150; i++) {
+			if (counters[i] != 0) {
+
+				System.out.println((char) i + " = " + counters[i]);
+			}
+		}
+	}
+
+}
